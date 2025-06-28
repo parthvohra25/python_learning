6193ef2cdf6e465bd35a8cd393bf3975b716ba64
Parth Vohra <72933441+parthvohra25@users.noreply.github.com>
2025-06-28 14:35:04 +0530

    Create Assignemnt_01.py

---
+++ Assignemnt_01.py
@@ -0,0 +1,17 @@
+#Task 1
+
+p1 = int(input("Enter the first number: "))
+p2 = int(input("Enter the second number: "))
+
+print("Addition: ",p1+p2)
+print("Subtraction: ",p1-p2)
+print("Multiplication: ",p1*p2)
+print("Division: ",p1/p2)
+
+
+#Task 2
+
+p1 = input("Enter your first name: ")
+p2 = input("Enter your last name: ")
+
+print("Hello,"+p1+ " " +p2 + "! Welcome to the Python program")
